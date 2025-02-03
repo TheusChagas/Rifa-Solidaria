39b15926335059e8d3d31942380afa4cfed26711
Victoriasilvagit <victoriadoesantosilva@gmail.com>
2025-02-03 19:09:05 -0300

    atualizacao login e criação registro

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,44 +1,97 @@
 'use client'
 
 import React from 'react'
-import { Input } from '@/components/ui/input' 
 import Image from 'next/image'
 import Logo from "@/assets/Logo.png"
+import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { Checkbox } from '@/components/ui/checkbox'
 
-function page() {
-  return (
-    <div className='items-center space-y-4 md:mt-[5%] mt-[40%]'>
-      
-      <div className=' justify-self-center'>
-        <Image src={Logo} alt='Logo' className='w-24 h-24 md:w-32 md:h-32' priority={true} ></Image>
-        <img src="assets/logo rifa sem fundo.png" alt="" />
-      </div>
+const formSchema = z.object({
+  email: z.string().email(),
+  senha: z.string().min(8).max(20),
+});
+export default function Page() {
+  // 1. Defina o formulário
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      senha: "",
+    },
+  })
+ 
+  // 2. Defina o manipulador de envio
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
 
-      <div className='m-5  justify-self-center w-1/2 space-y-1  '>
-          <Input type="email" placeholder='Email' className='' />
-          <Input type="password" placeholder='Senha' className='' />
-      </div>
-      <div className='md:justify-self-center ml-28 md:ml-0'>
-        <div className='sm:flex sm:space-x-[150px] md:space-x-[275px] xl:space-x-[400px]'>
-          <div className='flex items-center space-x-2'>
-            <Checkbox id="remember" className='data-[state=checked]:bg-verde-600' ></Checkbox>
-            <label htmlFor="remember" id='lembrar'>Lembrar Senha</label>
+  return (
+    <div className='md:w-[50%] justify-self-center mt-8'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 p-5">
+          <div className='items-center justify-self-center'>
+          <Image src={Logo} alt='Logo' className='w-24 h-24 md:w-32 md:h-32' priority={true} ></Image>
+          <img src="assets/logo rifa sem fundo.png" alt="" />
+
           </div>
-          <label htmlFor="forget" className='text-verde-600 font-semibold hover:text-verde-700'>Trocar Senha</label>
-        </div>
-      </div>
-      <div className='justify-self-center w-1/2'>
-        <button className='bg-verde-600 hover:bg-verde-700 p-2 w-full rounded-lg  text-white font-bold'>
-          Entrar
-        </button>
-      </div>
-      <div className='justify-self-center '>
-      <a href="" className='text-gray-600 font-semibold'>Não possui conta ainda?</a>
-      </div>
-      
+         
+          
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel></FormLabel>
+                <FormControl>
+                  <Input placeholder="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="senha"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel></FormLabel>
+                <FormControl>
+                  <Input placeholder="Senha" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className='text-gray-500 font-medium md:flex items-center md:justify-between pt-4'>
+            <div className='space-x-1'>
+            <Checkbox id="remember" className='data-[state=checked]:bg-verde-600 '></Checkbox>
+            <label htmlFor="remember" id='lembrar'>Lembrar senha</label>
+            </div>
+            
+            <p>Trocar senha</p>
+          </div>
+          <div className='flex items-center flex-col space-y-4'>
+            <Button type="submit" className='bg-verde-600 hover:bg-verde-700 p-2  w-full rounded-lg  text-white font-bold'>Entrar</Button>
+            <a className='text-gray-500 font-medium' href='register'>Não possui conta ainda?</a>
+          </div>
+        </form>
+      </Form>
     </div>
   )
 }
-
-export default page
